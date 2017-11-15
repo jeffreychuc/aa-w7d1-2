8b7d062b8b21f568b7fe00283911420bad6fe2d0
Jeffrey Chuc <jeffreychuc@gmail.com>
2017-11-14 17:06:22 -0800

    reorganized

--- frontend/store/store.js
+++
@@ -1,7 +0,0 @@
-import { createStore, applyMiddleware } from "redux";
-import rootReducer from '../reducers/root_reducer.js'
-import logger from 'redux-logger';
-
-const configureStore = createStore(rootReducer, applyMiddleware(logger));
-
-export default configureStore;
